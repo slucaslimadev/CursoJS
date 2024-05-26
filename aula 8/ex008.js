97dd225c98a485905755851d7e71f5a89d389308
slucaslimadev <lucaslima.rec@gmail.com>
2024-05-26 14:49:01 -0300

    exercicios JS

---
+++ aula 8/ex008.js
@@ -0,0 +1,8 @@
+var vel = 5
+console.log(`a velocidade do seu carro é de ${vel} KM/H`)
+if (vel > 60) { 
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+if (vel <25) {
+    console.log(`Você está abaixo da velocidade permitida. Acelere`)
+}
